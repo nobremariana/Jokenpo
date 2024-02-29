d248dac3f39731156107ce166ece2c069285295e
nobremariana <nobremariana9@gmail.com>
2024-02-29 13:21:39 -0300

    finalizado

--- scripts.js
+++ scripts.js
@@ -1,21 +1,24 @@
 const result = document.querySelector('.result')
 const humanScore = document.querySelector('#human-score')
 const machineScore = document.querySelector("#machine-scores")
-const ButtonYour= document.querySelector(".button-your")
-const Buttonmachine= document.querySelector(".button-machine")
+const buttonYour = document.querySelector(".button-your span")
+const buttonMachine = document.querySelector(".button-machine span")
 
 let humanScoreNumber = 0
 let machineScoreNumber = 0
 
-
 const PlayHuman = (humanChoice) => {
-    PlayTheGame(humanChoice, PlayMachine())
+    const machineChoice = PlayMachine()
+    PlayTheGame(humanChoice, machineChoice)
+
+    // Atualiza os emojis escolhidos
+    buttonYour.innerHTML = getEmoji(humanChoice)
+    buttonMachine.innerHTML = getEmoji(machineChoice)
 }
 
 const PlayMachine = () => {
     const choices = ['rock', 'paper', 'scissors']
     const randomNumber = Math.floor(Math.random() * 3)
-
     return choices[randomNumber]
 }
 
@@ -26,14 +29,27 @@ const PlayTheGame = (human, machine) => {
         result.innerHTML = "Deu empate 😐"
     } else if ((human === 'paper' && machine === 'rock') ||
         (human === 'scissors' && machine === 'paper') ||
-        (human === 'paper' && machine === 'rock')) {
-           
-            humanScoreNumber++
-           humanScore.innerHTML = humanScoreNumber
-            result.innerHTML = 'Você ganhou! 🥳'
-    } else{
+        (human === 'rock' && machine === 'scissors')) {
+        humanScoreNumber++
+        humanScore.innerHTML = humanScoreNumber
+        result.innerHTML = 'Você ganhou! 🥳'
+    } else {
         machineScoreNumber++
         machineScore.innerHTML = machineScoreNumber
         result.innerHTML = 'Você perdeu! 😢'
+    }
+}
+
+// Função auxiliar para obter o emoji correspondente à escolha
+const getEmoji = (choice) => {
+    switch (choice) {
+        case 'rock':
+            return '&#x1F44A';
+        case 'paper':
+            return '&#x270B';
+        case 'scissors':
+            return '&#x270C';
+        default:
+            return '';
     }
 }
